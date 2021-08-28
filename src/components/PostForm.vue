e0625807affe02d595318c80b4a5dadbd36cf918
OgievichV <Ogievichvo@gmail.com>
2021-08-28 18:46:14 +0300

    Project created

---
+++ src/components/PostForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <input
+      v-model="post.title"
+      type="text"
+      placeholder="Название"
+    />
+    <input
+      v-model="post.body"
+      type="text"
+      placeholder="Описание"
+    />
+    <button
+      style="align-self: flex-end; margin-top: 15px"
+      @click="createPost"
+    >
+      Создать
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'PostList',
+  components: {},
+  data() {
+    return {
+      post: {
+        title: '',
+        body: ''
+      }
+    }
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit('create', this.post)
+      this.post = {
+        title: '',
+        body: ''
+      }
+    }
+  },
+}
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div 
+    class="post" 
+    v-for="post in posts" 
+    :key="post.id"
+  >
+    <div><strong>Название:</strong> {{post.title}}</div>
+    <div><strong>Описание:</strong> {{post.body}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'PostList',
+  components: {},
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }, 
+
+  data() {
+    return {}
+  },
+  mounted() {},
+  beforeDestroy() {},
+  methods: {}
+}
+</script>
+
+<style lang="css" scoped>
+
+.post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+}
+
+</style>
