5ad0f0a61f8c783b99dae7c361b58453be758d2f
OgievichV <Ogievichvo@gmail.com>
2021-08-30 18:25:30 +0300

    delete post

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,16 @@
 <template>
-<div>
+<div v-if="posts.length > 0">
   <h3>Срисок пользователей</h3>
   <post-item
     v-for="post in posts" 
     :post="post"
     :key="post.id"
+    @remove="$emit('remove', post)"
   />
 </div>
+<h2 v-else style="color: red">
+Список пользователей пуст
+</h2>
 </template>
 
 <script>
